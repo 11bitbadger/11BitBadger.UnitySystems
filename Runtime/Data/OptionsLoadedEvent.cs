8e9a5ad92c77c79313b61450a13cc62131313446
11bitbadger <social@11bitbadger.com>
2021-04-23 17:38:56 +0100

    Add data systems.
     * Encrypted file save/load system.
     * Options system with events.

---
+++ Runtime/Data/OptionsLoadedEvent.cs
@@ -0,0 +1,19 @@
+namespace Badger.Systems
+{
+    public class OptionsLoadedEvent : IEventAggregatorEvent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public OptionsSystem OptionsSystem { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="optionsSystem"></param>
+        public OptionsLoadedEvent(OptionsSystem optionsSystem)
+        {
+            OptionsSystem = optionsSystem;
+        }
+    }
+}
